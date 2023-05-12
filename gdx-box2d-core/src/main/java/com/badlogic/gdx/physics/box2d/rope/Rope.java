e033b1f54074d3fde6f105536e1aee39ceefd0ab
ice1000 <ice1000kotlin@foxmail.com>
2023-05-11 21:10:17 -0400

    Maybe stdc++-6 works?

--- gdx-box2d-core/src/main/java/com/badlogic/gdx/physics/box2d/rope/Rope.java
+++ gdx-box2d-core/src/main/java/com/badlogic/gdx/physics/box2d/rope/Rope.java
@@ -1,4 +1,18 @@
 package com.badlogic.gdx.physics.box2d.rope;
 
+/**
+ * b2_rope.h
+ *
+ * @author ice1000
+ */
 public class Rope {
+    // @off
+	/*JNI
+#include <box2d/box2d.h>
+	 */
+    protected long addr;
+
+    public Rope(long addr) {
+        this.addr = addr;
+    }
 }
